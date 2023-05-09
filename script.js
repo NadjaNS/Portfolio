81038855d116ab796c9e274d15e28378bcc16d55
Nadja <100866219+NadjaNS@users.noreply.github.com>
2023-05-09 21:07:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const menuBtn = document.querySelector(`.menu-btn`);
+const menu = document.querySelector(`.menu`);
+const menuNav = document.querySelector(`.menu-nav`);
+const menuImg = document.querySelector(`.menu-image`);
+const navItems = document.querySelectorAll(`.menu-item`);
+
+//se initial set of menu
+let showMenu = false;
+menuBtn.addEventListener('click', toggleMenu);
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add(`close`);
+    menu.classList.add(`show`);
+    menuNav.classList.add(`show`);
+    menuImg.classList.add(`show`);
+    navItems.forEach((item) => item.classList.add(`show`));
+
+    // set menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove(`close`);
+    menu.classList.remove(`show`);
+    menuNav.classList.remove(`show`);
+    menuImg.classList.remove(`show`);
+    navItems.forEach((item) => item.classList.remove(`show`));
+    showMenu = false;
+  }
+}
